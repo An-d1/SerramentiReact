8aee65afecc2180565ae31c74aec2c9b6e5f217e
Andis <andisbara@gmail.com>
2024-11-30 16:12:21 +0100

    First Commit , added navbar

---
+++ beni-serramenti/src/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import './navbar.css'
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg">
+  <div className="container-fluid">
+      <a className="navbar-brand" href='{#}'>BENI</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse me-5" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-link me-3" aria-current="page" href="{#}">Home</a>
+        <a className="nav-link me-3" href="{#}">Prodotti</a>
+        <a className="nav-link me-3" href="{#}">Contact</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+  );
+}
+
+export default Navbar;
